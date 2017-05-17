d5dc56548496da4dbef1fb3aa85b79c6b63034ff
wkasem2 <wkasem22@gmail.com>
2017-05-17 17:34:14 +0200

    columns zy l 5ra

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -6,10 +6,17 @@ class Menu extends Model
 {
 
   protected  $table = 'menu';
-    public static function all($id)
+    
+   public static function all($id)
    {
     $instance = new static;
-    $data = $instance->stream->query("SELECT meal.* FROM menu INNER JOIN meal ON menu.mealid = meal.idmeal where resurantid =$id ");
+    $data = $instance->stream
+            ->query("
+               SELECT menue.* , meal.* ,categore.name FROM menu m
+                  INNER JOIN meal ON m.mealid = meal.idmeal 
+                  INNER JOIN categore ON meal.categoreid = categore.idcategore 
+               where menue.resurantid = $id ");
+       
     return $data->fetchAll();
    }
 }
